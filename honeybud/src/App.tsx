71a9fb79da0f747eb333bf0913f90a5986319b72
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-10-01 11:50:48 -0300

    fixed imports

--- honeybud/src/App.tsx
+++ honeybud/src/App.tsx
@@ -1,33 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './assets/font/ReemKufi-Regular.ttf';
-import './assets/font/ReemKufi-Medium.ttf';
-import './assets/font/ReemKufi-Bold.ttf';
-import {Header} from './components/header/Header'
-import {Banner} from './components/banner/Banner'
-import {Strip} from './components/strip/Strip'
-import {Catalog} from './components/catalog/Catalog'
-import {Credits} from './components/credits/Credits'
+import '../assets/font/ReemKufi-Regular.ttf';
+import '../assets/font/ReemKufi-Medium.ttf';
+import '../assets/font/ReemKufi-Bold.ttf';
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
 
 
 function App() {
   return (
-    <div className="FrontPage">
-      <div className= "FixedHeader">
-        <Header />
-      </div>
-        <Banner />
-        <div className= "LargeStrips">
-          <Strip />
-          <Strip />
-        </div>
-        <div className= "EndofPage">
-          <Catalog />
-          <Credits />
-        </div>
-      </div>
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+      </>
   );
 }
+
 
 export default App;
---
+++ honeybud/src/pages/LandingPage.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../assets/font/ReemKufi-Regular.ttf';
+import '../assets/font/ReemKufi-Medium.ttf';
+import '../assets/font/ReemKufi-Bold.ttf';
+import {Header} from '../components/header/Header'
+import {Banner} from '../components/banner/Banner'
+import {Strip} from '../components/strip/Strip'
+import {Catalog} from '../components/catalog/Catalog'
+import {Credits} from '../components/credits/Credits'
+
+
+function LandingPage() {
+  return (
+    <div className="FrontPage">
+      <div className= "FixedHeader">
+        <Header />
+      </div>
+        <Banner />
+        <div className= "LargeStrips">
+          <Strip />
+          <Strip />
+        </div>
+        <div className= "EndofPage">
+          <Catalog />
+          <Credits />
+        </div>
+      </div>
+  );
+}
+
+export default LandingPage;
